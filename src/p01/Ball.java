2b89b0318d82422eb888705f147e44a7332dccea
Heber94 <heber_hsc@hotmail.com>
2017-03-07 20:04:22 +0100

    Commit con el contenido proporcionado por el profesor.

---
+++ src/p01/Ball.java
@@ -0,0 +1,81 @@
+package p01;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+//TODO Transform the code to be used safely in a concurrent context.  
+public class Ball {
+       //TODO  Find an archive named Ball.png 
+	private String Ball = "Ball.png"; 
+
+	private double x,y,dx,dy;
+	private double v,fi;
+	private Image image;
+
+	public Ball() {
+		ImageIcon ii = new ImageIcon(this.getClass().getResource(Ball));
+		image = ii.getImage();
+		x = Billiards.Width/4-16;
+		y = Billiards.Height/2-16;
+		v = 5;
+		fi =  Math.random() * Math.PI * 2;
+	}
+
+	public void move() {
+		v = v*Math.exp(-v/1000);
+		dx = v*Math.cos(fi);
+		dy = v*Math.sin(fi);
+		if (Math.abs(dx) < 1 && Math.abs(dy) < 1) {
+			dx = 0;
+			dy = 0;
+		}
+		x += dx;   
+		y += dy;
+		//TODO Check postcondition
+	}
+
+	public void reflect() {
+		if (Math.abs(x + 32 - Board.RIGHTBOARD) <  Math.abs(dx)) {
+			fi = Math.PI - fi;
+		}
+		if (Math.abs(y + 32 - Board.BOTTOMBOARD) <  Math.abs(dy)) {
+			fi = - fi;
+		}
+		if (Math.abs(x - Board.LEFTBOARD) <  Math.abs(dx)) {
+			fi = Math.PI - fi;
+		}
+		if (Math.abs(y - Board.TOPBOARD) <  Math.abs(dy)) {
+			fi = - fi;
+		}
+		//TODO Check postcondition	
+	}
+
+	public int getX() {
+		return (int)x;
+	}
+	
+	public int getY() {
+		return (int)y;
+	}
+	
+	public double getFi() {
+		return fi;
+	}
+
+	public double getdr() {
+		return Math.sqrt(dx*dx+dy*dy);
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public Image getImage() {
+		return image;
+	}
+
+}
+
